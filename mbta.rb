9ab367d4b5b222b389b415b656818f5ba6957dd4
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-09-25 17:59:09 -0400

    just starting out

--- mbta.rb
+++ mbta.rb
@@ -1,1 +1,32 @@
-# Your code goes here
+#The key in each hash is the stop on the MBTA line
+#The value for each key is the number of stops you visit to get from the MBTA stop specified in the key to park street
+
+Green_Line = {
+    "Haymarket" => 3,
+    "Government Center" => 2,
+    "Park"=> 1,
+    "Bolyston"=> 2,
+    "Arlington"=> 3,
+    "Copley"=> 4,
+}
+
+Orange_Line = {
+    "South Station" => 2,
+    "Park" => 1,
+    "Kendall"=> 1,
+    "Central"=> 2,
+    "Harvard"=> 3,
+    "Porter"=> 4,
+    "Davis" => 3,
+    "Alewife" => 2,
+}
+
+Red_Line = {
+    "Haymarket" => 3,
+    "Government Center" => 2,
+    "Park"=> 1,
+    "Bolyston"=> 2,
+    "Arlington"=> 3,
+    "Copley"=> 4,
+}
+
