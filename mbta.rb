17bb731db2c9228c6e9c68eefcd94305486a5ba1
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-09-25 19:59:39 -0400

    I converted all the string within the hash to symbols

--- mbta.rb
+++ mbta.rb
@@ -2,71 +2,70 @@
 #The value for each key is the number of stops you visit to get from the MBTA stop specified in the key to park street
 #VARS Initial_Stop ; Initial_Line ; Final_Stop ; Final_Line
 #Input
-"Enter Initial Stop: "
-Initial_Stop = gets.chomp
+puts "Enter Initial Stop: "
+Initial_Stop = gets.chomp.to_sym
 
-"Enter Initial Line: "
-Initial_Line = gets.chomp
+puts "Enter Initial Line: "
+Initial_Line = gets.chomp.to_sym
 
-"Enter Final Stop: "
-Final_Stop = gets.chomp
+puts "Enter Final Stop: "
+Final_Stop = gets.chomp.to_sym
 
-"Enter Final Line: "
-Final_Line = gets.chomp
+puts "Enter Final Line: "
+Final_Line = gets.chomp.to_sym
 
 #Computation
 
-user_input = [Initial_Stop, Initial_Line, Final_Stop, Final_Line]
 
 #This Conditional Statement will evaluate which MBTA Line Hash to look at
-If Initial_Line == "Green"
-	Green_Line
 
+Mbta_Lines = {
 
+	green = {
+	    haymarket: 2,
+	    government_center: 1,
+	    park: 0,
+	    bolyston: 1,
+	    arlington: 2,
+	    copley: 3,
+	},
 
+	red = {
+	    south_station: 1,
+	    park: 0,
+	    kendall: 1,
+	    central: 2,
+	    harvard: 3,
+	    porter: 4,
+	    davis: 5,
+	    alewife: 6,
+	},
 
+	orange = {
+	    north_station: 2,
+	    haymarket: 1,
+	    park: 0,
+	    downtown_crossing: 1,
+	    chinatown: 2,
+	    back_bay: 3,
+	    forest_hills: 4,
+	},
 
-
-
-
-
-Green_Line = {
-    "Haymarket" => 2,
-    "Government Center" => 1,
-    "Park"=> 0,
-    "Bolyston"=> 1,
-    "Arlington"=> 2,
-    "Copley"=> 3,
 }
-
-Red_Line = {
-    "South Station" => 1,
-    "Park" => 0,
-    "Kendall"=> 1,
-    "Central"=> 2,
-    "Harvard"=> 3,
-    "Porter"=> 4,
-    "Davis" => 5,
-    "Alewife" => 6,
-}
-
-Orange_Line = {
-    "North Station" => 2,
-    "Haymarket" => 1,
-    "Park"=> 0,
-    "Downtown Crossing"=> 1,
-    "Chinatown"=> 2,
-    "Back Bay"=> 3,
-    "Forest Hills"=> 4,
-}
-
-
+#distance_a will be the first leg of the trip; distance_b will be the second leg of the trip
+distance_a = Mbta_Lines[:Initial_Line][:Initial_Stop]
+distance_b = Mbta_Lines[:Final_Line][:Final_Stop]
 #T_lines = [Green_Line, Red_Line, Orange_Line]
 #I will use the distance method to calculate the number of stops each trip will take
+
 def distance(point_a , point_b)
 	return point_a + point_b + 1
 end
 
+Number_of_Stops = distance(distance_a, distance_b)
+
 
 #output
-puts distance(2,3)
+
+
+Puts "Your total number of stops will be #{Number_of_Stops}"
