b6f246f962b1bc365974a8d1fa3c7eb5d7260026
Rahul Horé <hore.rahul@gmail.com>
2013-09-25 18:09:39 -0400

    Adds prompts for user input

--- mbta.rb
+++ mbta.rb
@@ -7,7 +7,15 @@ orange_line = ['north station', 'haymarket', 'park st', 'state',
 
 mbta_subway_system = { red: red_line, green: green_line, orange: orange_line }
 
-def number_of_stops(line_on, start, line_off, stop)
-  # code goes here
+def number_of_stops(line_on, point_of_departure, line_off, destination)
+  
 end
 
+print "What line are you taking? "
+line_on = gets.chomp.downcase!
+print "What is your point of departure? "
+point_of_departure = gets.chomp.downcase!
+print "What line are you getting off on? "
+line_off = gets.chomp.downcase!
+print "What is your destination? "
+destination = gets.chomp.downcase!
