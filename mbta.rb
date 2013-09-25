1f798eab7953570a7a2e3138f58f4ed2ea91f734
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-09-25 18:54:31 -0400

    This is my commit before I attempt in combining all of my mbta hashes into another hash

--- mbta.rb
+++ mbta.rb
@@ -1,6 +1,35 @@
 #The key in each hash is the stop on the MBTA line
 #The value for each key is the number of stops you visit to get from the MBTA stop specified in the key to park street
 #VARS Initial_Stop ; Initial_Line ; Final_Stop ; Final_Line
+#Input
+"Enter Initial Stop: "
+Initial_Stop = gets.chomp
+
+"Enter Initial Line: "
+Initial_Line = gets.chomp
+
+"Enter Final Stop: "
+Final_Stop = gets.chomp
+
+"Enter Final Line: "
+Final_Line = gets.chomp
+
+#Computation
+
+user_input = [Initial_Stop, Initial_Line, Final_Stop, Final_Line]
+
+#This Conditional Statement will evaluate which MBTA Line Hash to look at
+If Initial_Line == "Green"
+	Green_Line
+
+
+
+
+
+
+
+
+
 Green_Line = {
     "Haymarket" => 2,
     "Government Center" => 1,
@@ -31,5 +60,13 @@ Orange_Line = {
     "Forest Hills"=> 4,
 }
 
-Stop_num = firsti
+
 #T_lines = [Green_Line, Red_Line, Orange_Line]
+#I will use the distance method to calculate the number of stops each trip will take
+def distance(point_a , point_b)
+	return point_a + point_b + 1
+end
+
+
+#output
+puts distance(2,3)
