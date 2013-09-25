2b0b20753241598bd8f106abb52b278b2e2961de
Rahul Horé <hore.rahul@gmail.com>
2013-09-25 17:57:10 -0400

    Creates hash of arrays to represent MBTA subway system

--- mbta.rb
+++ mbta.rb
@@ -1,1 +1,13 @@
-# Your code goes here
+red_line =    ['south station', 'park st', 'kendall', 'central',
+              'harvard', 'porter', 'davis', 'alewife']
+green_line =  ['haymarket', 'government center', 'park st', 'boylston',
+              'arlington', 'copley']
+orange_line = ['north station', 'haymarket', 'park st', 'state',
+              'downtown crossing', 'chinatown', 'back bay', 'forest hills']
+
+mbta_subway_system = { red: red_line, green: green_line, orange: orange_line }
+
+def number_of_stops(line_on, start, line_off, stop)
+  # code goes here
+end
+
