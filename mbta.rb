bd109bc691c483e5fb3424d9209f8d46ddca57c8
Rahul Horé <hore.rahul@gmail.com>
2013-09-25 19:21:32 -0400

    Finds the total number of stops on the same line

--- mbta.rb
+++ mbta.rb
@@ -1,21 +1,35 @@
+require 'pry'
+
 red_line =    ['south station', 'park st', 'kendall', 'central',
-              'harvard', 'porter', 'davis', 'alewife']
+               'harvard', 'porter', 'davis', 'alewife']
 green_line =  ['haymarket', 'government center', 'park st', 'boylston',
-              'arlington', 'copley']
+               'arlington', 'copley']
 orange_line = ['north station', 'haymarket', 'park st', 'state',
-              'downtown crossing', 'chinatown', 'back bay', 'forest hills']
+               'downtown crossing', 'chinatown', 'back bay', 'forest hills']
 
-mbta_subway_system = { red: red_line, green: green_line, orange: orange_line }
+MBTA_SUBWAY_SYSTEM = { red: red_line, green: green_line, orange: orange_line }
 
-def number_of_stops(line_on, point_of_departure, line_off, destination)
-  
+def calculate_position_of_stop(line, stop)
+  MBTA_SUBWAY_SYSTEM[line.to_sym].index(stop)
+end
+
+def calculate_number_of_stops(line_on, point_of_departure, line_off, destination)
+  return (calculate_position_of_stop(line_on, point_of_departure) -
+          calculate_position_of_stop(line_off, destination)).abs if line_on == line_off
 end
 
 print "What line are you taking? "
-line_on = gets.chomp.downcase!
+line_on = gets.chomp.downcase
 print "What is your point of departure? "
-point_of_departure = gets.chomp.downcase!
+point_of_departure = gets.chomp.downcase
 print "What line are you getting off on? "
-line_off = gets.chomp.downcase!
+line_off = gets.chomp.downcase
 print "What is your destination? "
-destination = gets.chomp.downcase!
+destination = gets.chomp.downcase
+
+# puts line_on, point_of_departure, line_off, destination
+
+total_number_of_stops = calculate_number_of_stops(line_on, point_of_departure, 
+                                                  line_off, destination)
+
+puts "There are a total of #{total_number_of_stops} stops to your destination."
