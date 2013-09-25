7d31755bc953127990b2d06dd47ae8fd62cb1292
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-09-25 18:18:49 -0400

    I have now outlined each T line in a hash alone with the Park_Value of each stop. Park_Value is the distance each stop is from Park street.

--- mbta.rb
+++ mbta.rb
@@ -1,32 +1,35 @@
 #The key in each hash is the stop on the MBTA line
 #The value for each key is the number of stops you visit to get from the MBTA stop specified in the key to park street
-
+#VARS Initial_Stop ; Initial_Line ; Final_Stop ; Final_Line
 Green_Line = {
-    "Haymarket" => 3,
-    "Government Center" => 2,
-    "Park"=> 1,
-    "Bolyston"=> 2,
-    "Arlington"=> 3,
-    "Copley"=> 4,
+    "Haymarket" => 2,
+    "Government Center" => 1,
+    "Park"=> 0,
+    "Bolyston"=> 1,
+    "Arlington"=> 2,
+    "Copley"=> 3,
 }
 
-Orange_Line = {
-    "South Station" => 2,
-    "Park" => 1,
+Red_Line = {
+    "South Station" => 1,
+    "Park" => 0,
     "Kendall"=> 1,
     "Central"=> 2,
     "Harvard"=> 3,
     "Porter"=> 4,
-    "Davis" => 3,
-    "Alewife" => 2,
+    "Davis" => 5,
+    "Alewife" => 6,
 }
 
-Red_Line = {
-    "Haymarket" => 3,
-    "Government Center" => 2,
-    "Park"=> 1,
-    "Bolyston"=> 2,
-    "Arlington"=> 3,
-    "Copley"=> 4,
+Orange_Line = {
+    "North Station" => 2,
+    "Haymarket" => 1,
+    "Park"=> 0,
+    "Downtown Crossing"=> 1,
+    "Chinatown"=> 2,
+    "Back Bay"=> 3,
+    "Forest Hills"=> 4,
 }
 
+Stop_num = firsti
+#T_lines = [Green_Line, Red_Line, Orange_Line]
