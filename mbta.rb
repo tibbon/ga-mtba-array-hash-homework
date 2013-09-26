15f65ae4a87e49315221519e9a404c01566a3947
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-25 22:01:16 -0400

    Getting wrong number of stops

--- mbta.rb
+++ mbta.rb
@@ -2,36 +2,36 @@
 
 #MBTA hash with keys as symbols of lines and values as arrays of stops
 MBTA = {
-	red_line: ["south_station", "park_st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
-	green_line: ["haymarket_green", "government_center", "park_st", "boylston", "arlington", "copley"],
-	orange_line: ["north_station", "haymarket", "park_st", "state", "downtown_crossing", "chinatown", "back_bay", "forest_hills"]
+	red: ["south_station", "park_st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+	green: ["haymarket_green", "government_center", "park_st", "boylston", "arlington", "copley"],
+	orange: ["north_station", "haymarket", "park_st", "state", "downtown_crossing", "chinatown", "back_bay", "forest_hills"]
 }
 
 puts "Welcome to the MBTA! Where are you going?"
 destination = gets.chomp
 puts "What line is that on?"
-destination_line = gets.chomp
+destination_line = gets.chomp.to_sym
 puts "What station are you starting from?"
 home = gets.chomp
 puts "And which line is that on?"
-home_line = gets.chomp
+home_line = gets.chomp.to_sym
 
 def same_line(home_line, home, destination_line, destination)
-		return home_index = MBTA[home_line.to_sym].index(home).to_i
-		return destination_index = MBTA[destination_line.to_sym].index(destination).to_i
-		return (home_index - destination_index).abs
+	return home_index = MBTA[home_line].index(home).to_i
+	return destination_index = MBTA[destination_line].index(destination).to_i
+	return (home_index - destination_index).abs
 end
 
-
 def diff_lines(home_line, home, destination_line, destination)
-	return distance_one = MBTA[home_line.to_sym].index(home).to_i
-	return distance_two = MBTA[destination_line.to_sym].index(destination).to_i
+	return home_index = MBTA[home_line].index(home).to_i
+	return destination_index = MBTA[destination_line].index(destination).to_i
 
-	return home_mid_point = MBTA[home_line.to_sym].index("park_st").to_i
-	return destination_mid_point = MBTA[home_line.to_sym].index("park_street").to_i
+	return home_mid_point = MBTA[home_line].index("park_st").to_i
+	return destination_mid_point = MBTA[destination_line].index("park_st").to_i
 
-	return first_leg = (distance_one - home_mid_point).abs.to_i
-	return second_leg = (distance_two - destination_mid_point).abs.to_i
+	return first_leg = (home_index - home_mid_point).abs.to_i
+	return second_leg = (destination_index - destination_mid_point).abs.to_i
+	return first_leg + second_leg
 end
 
 if home_line == destination_line
@@ -39,15 +39,3 @@ if home_line == destination_line
 else
 	puts "You have #{diff_lines(home_line, home, destination_line, destination)} stops until your destination."
 end
-
-
-# number_of_stops = calc_stops
-
-# 	puts "You have #{number_of_stops} stops until your destination."
-
-# if startline == endline
-#     puts (startline.index(destination)-startline.index(location)).abs
-# else
-#     puts "You have #{(subway[:startline].index(location) - subway[:startline].index("unionsquare")).abs} stops until transfer"
-#     puts "After you have #{(subway[:endline].index(destination) - subway[:endline]index("unionsquare")).abs} until your destination"    
-# end    
