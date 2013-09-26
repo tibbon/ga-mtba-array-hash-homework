ce324161ba8f5813c2cb2b406925da25182f3cf4
Matt Clement <darkmoves@gmail.com>
2013-09-25 20:09:59 -0400

    MBTA file improvements.

--- mbta.rb
+++ mbta.rb
@@ -1,1 +1,19 @@
-# Your code goes here
+require 'pry'
+
+
+
+red = ["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis"]
+
+# Inputs
+
+
+def subway(geton, getoff)
+	trip_length = (geton - getoff).abs
+	return trip_length
+end
+
+trip_length = subway(1, 3)
+puts trip_length
+
+trip_length = subway(6, 2)
+puts trip_length
