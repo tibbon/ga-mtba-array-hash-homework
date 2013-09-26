58cbb5788326487b7bfa26b7f8cc1fc205db357a
Cori Allen <coriallen@gmail.com>
2013-09-26 05:31:32 -0400

    Create subway line hash of subway stops arrays.

--- mbta.rb
+++ mbta.rb
@@ -1,1 +1,6 @@
 # Your code goes here
+subway_lines {
+	orange: ["north station", "orange haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"],
+	green: ["green haymarket", "government center", "park st", "boylston", "arlington", "copley"],
+	red: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+}
