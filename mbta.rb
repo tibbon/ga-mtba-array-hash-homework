d182b58b2a7882a83b2af34beec4d7a2f7a72d2a
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-25 21:42:49 -0400

    Closing in on an answer

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,5 @@
 # Your code goes here
-require 'pry'
+
 #MBTA hash with keys as symbols of lines and values as arrays of stops
 MBTA = {
 	red_line: ["south_station", "park_st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
@@ -7,7 +7,6 @@ MBTA = {
 	orange_line: ["north_station", "haymarket", "park_st", "state", "downtown_crossing", "chinatown", "back_bay", "forest_hills"]
 }
 
-#user input: 4 items- home and destination, direction traveling?
 puts "Welcome to the MBTA! Where are you going?"
 destination = gets.chomp
 puts "What line is that on?"
@@ -17,27 +16,34 @@ home = gets.chomp
 puts "And which line is that on?"
 home_line = gets.chomp
 
-#define function to calc distance
-
-def calc_each_index(line, stop)
-	MBTA[line.to_sym].index(stop)
-end
-
 def same_line(home_line, home, destination_line, destination)
-		return (calc_index(home_line, home) - calc.index(destination_line, destination)).abs
+		return home_index = MBTA[home_line.to_sym].index(home).to_i
+		return destination_index = MBTA[destination_line.to_sym].index(destination).to_i
+		return (home_index - destination_index).abs
 end
 
 
 def diff_lines(home_line, home, destination_line, destination)
-	return ((calc_index(home_line, home) - calc_index(home_line, "park_st")).abs) + (calc_index(destination_line, "park_st") - (calc.index(destination_line, destination).abs)
+	return distance_one = MBTA[home_line.to_sym].index(home).to_i
+	return distance_two = MBTA[destination_line.to_sym].index(destination).to_i
+
+	return home_mid_point = MBTA[home_line.to_sym].index("park_st").to_i
+	return destination_mid_point = MBTA[home_line.to_sym].index("park_street").to_i
+
+	return first_leg = (distance_one - home_mid_point).abs.to_i
+	return second_leg = (distance_two - destination_mid_point).abs.to_i
 end
 
 if home_line == destination_line
-	same_line(MBTA, home_line)
+	puts "You have #{same_line(home_line, home, destination_line, destination)} stops until your destination."
+else
+	puts "You have #{diff_lines(home_line, home, destination_line, destination)} stops until your destination."
+end
 
-number_of_stops = calc_stops
 
-	puts "You have #{number_of_stops} stops until your destination."
+# number_of_stops = calc_stops
+
+# 	puts "You have #{number_of_stops} stops until your destination."
 
 # if startline == endline
 #     puts (startline.index(destination)-startline.index(location)).abs
