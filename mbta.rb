080efdcf51b873148a1a29f2c9bdf43ec863e864
Rahul Horé <hore.rahul@gmail.com>
2013-09-25 23:29:22 -0400

    Refactors input, output, and calculations into distinct methods

--- mbta.rb
+++ mbta.rb
@@ -12,28 +12,41 @@ def calculate_position_of_stop(line, stop)
   MBTA_SUBWAY_SYSTEM[line.to_sym].index(stop)
 end
 
+def calculate_distance_between_stops(line_on, start, line_off, stop)
+  (calculate_position_of_stop(line_on, start) -
+   calculate_position_of_stop(line_off, stop)).abs
+end
+
 def calculate_number_of_stops(line_on, point_of_departure, line_off, destination)
   if line_on == line_off
-    return (calculate_position_of_stop(line_on, point_of_departure) -
-            calculate_position_of_stop(line_off, destination)).abs
+    return calculate_distance_between_stops(line_on, point_of_departure,
+                                            line_off, destination)
   else
-    return (calculate_position_of_stop(line_on,  point_of_departure) -
-            calculate_position_of_stop(line_on,  'park st')).abs + 
-           (calculate_position_of_stop(line_off, 'park st') -
-            calculate_position_of_stop(line_off, destination)).abs
+    return calculate_distance_between_stops(line_on,  point_of_departure,
+                                            line_on,  'park st') + 
+           calculate_distance_between_stops(line_off, 'park st',
+                                            line_off, destination)
   end
 end
 
-print "What line are you taking? "
-line_on = gets.chomp.downcase
-print "What is your point of departure? "
-point_of_departure = gets.chomp.downcase
-print "What line are you getting off on? "
-line_off = gets.chomp.downcase
-print "What is your destination? "
-destination = gets.chomp.downcase
+def get_user_input
+  print "What line are you taking? "
+  line_on = gets.chomp.downcase
+  print "What is your point of departure? "
+  point_of_departure = gets.chomp.downcase
+  print "What line are you getting off on? "
+  line_off = gets.chomp.downcase
+  print "What is your destination? "
+  destination = gets.chomp.downcase
+  return line_on, point_of_departure, line_off, destination
+end
 
-total_number_of_stops = calculate_number_of_stops(line_on, point_of_departure, 
-                                                  line_off, destination)
+def output_total_number_of_stops
+  line_on, point_of_departure, line_off, destination = *get_user_input
+  total_number_of_stops = calculate_number_of_stops(line_on, point_of_departure,
+                                                    line_off, destination)
 
-puts "The total number of stops to your destination riding from #{point_of_departure.capitalize} on the #{line_on.capitalize} Line to #{destination.capitalize} on the #{line_off.capitalize} Line is #{total_number_of_stops}."
+  "The total number of stops to your destination riding from #{point_of_departure.capitalize} on the #{line_on.capitalize} Line to #{destination.capitalize} on the #{line_off.capitalize} Line is #{total_number_of_stops}."
+end
+
+puts output_total_number_of_stops
