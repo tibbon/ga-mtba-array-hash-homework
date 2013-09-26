91b9d6b3d76febcedd8a529814479fdf98a2a51f
Cori Allen <coriallen@gmail.com>
2013-09-26 06:55:28 -0400

    Creates a test method with first hash.

--- mbta.rb
+++ mbta.rb
@@ -1,17 +1,28 @@
-# # Your code goes here
-# require 'pry'
+# Your code goes here
 
-# subway_lines = {
-# 	orange: ["north station", "orange haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"],
-# 	green: ["green haymarket", "government center", "park st", "boylston", "arlington", "copley"],
-# 	red: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
-# }
-
-# puts "What subway line will you start your trip on? (organge, green, red)"
-# begin_line = gets.chomp.to_sym
+subway_lines = {
+	orange: ["north station", "orange haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"],
+	green: ["green haymarket", "government center", "park st", "boylston", "arlington", "copley"],
+	red: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+}
 
 
+def a_method(argument)
+	if argument == "orange"
+		return subway_lines[:orange]
+	elsif argument == "green"
+		return subway_lines[:green]
+	elsif argument == "red"
+		return subway_lines[:red]
+	else
+		return "We don't have that in Boston, try another line."
+	end
+end 
 
+puts "What subway line will you start your trip on? (organge, green, red)"
+begin_line = gets.chomp.to_sym
+
+begin_stops = a_method(begin_line)
 # def get_stops(subway_lines)
 # 	stops = []
 # 	subway_lines.each do |subway_line|
@@ -21,36 +32,36 @@
 #  subway_lines = [begin_line]
 
 # binding.pry
-require 'pry'
+# require 'pry'
 
-Orange = {
-	stops: ["north station", "orange haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"],
-}
+# Orange = {
+# 	stops: ["north station", "orange haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"],
+# }
 
-Green = {
-	stops: ["green haymarket", "government center", "park st", "boylston", "arlington", "copley"],
+# Green = {
+# 	stops: ["green haymarket", "government center", "park st", "boylston", "arlington", "copley"],
 
-}
+# }
 
-Red = {
-	stops: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
-}
-
-
-def get_stops(subway_lines)
-	all_stops = []
-	subway_lines.each do |subway_line|
-		all_stops << subway_line[:stops]
-	end
-	return all_stops.flatten.uniq
-end
-
-# puts "What subway line will you start your trip on? (organge, green, red)"
-# begin_line = gets.chomp.to_s
+# Red = {
+# 	stops: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+# }
 
 
-subway_lines = [Orange, Green, Red]
+# def get_stops(subway_lines)
+# 	all_stops = []
+# 	subway_lines.each do |subway_line|
+# 		all_stops << subway_line[:stops]
+# 	end
+# 	return all_stops.flatten.uniq
+# end
 
-all_stops = get_stops(subway_lines)
+# # puts "What subway line will you start your trip on? (organge, green, red)"
+# # begin_line = gets.chomp.to_s
 
-binding.pry
+
+# subway_lines = [Orange, Green, Red]
+
+# all_stops = get_stops(subway_lines)
+
+# binding.pry
