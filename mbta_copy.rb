b7c353e9550f6193213565776a15709be8a85050
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-03 13:56:49 -0400

    I believe I have a working model

--- mbta_copy.rb
+++ mbta_copy.rb
@@ -1,7 +1,19 @@
 #I decided to create a hash of subway lines that contain the line as the key and the stops in array as values
+require 'pry'
+
+puts "Enter your starting line"
+line_1 = gets.chomp
+puts "Enter your starting stop"
+stop_1 = gets.chomp
+puts "Enter your final line"
+line_2 = gets.chomp
+puts "Enter your final stop"
+stop_2 = gets.chomp
+
 
 subway_lines = {
-		red => ['south station',
+
+		:red => ['south station',
 					'park street',
 					'kendall',
 					'central',
@@ -10,14 +22,14 @@ subway_lines = {
 					'davis',
 					'alewife'],
 
-		green => ['haymarket',
+		:green => ['haymarket',
 					'government center',
 					'park street', 
 					'boylston',
 					'arlington',
 					'copley'],
 		
-		orange => ['north station',
+		:orange => ['north station',
 					'haymarket',
 					'park street',
 					'state',
@@ -27,9 +39,19 @@ subway_lines = {
 					'forest hills']				
 				}
 
-###The following is my attempt in understanding hashes
-#To store all the reserve coffees we have in the database, we want to create 
-# a hash that stores the type of coffee as a string in the
-# key and its description in the value
-require 'pry'
+		#binding.pry
+
+#what methods do i need? I need a method to determine the num of stops on my trip
+#There are two cases, so I need two methods
+total_stops = 0
+def dist_same_line(line_1, stop_1, line_2, stop_2, array)
+	stop_1_distance_to_park = array[line_1.to_sym].index('park street') - array[line_1.to_sym].index(stop_1).abs
+	stop_2_distance_to_park = array[line_2.to_sym].index('park street') - array[line_2.to_sym].index(stop_2).abs
+	total_stops = stop_1_distance_to_park + stop_2_distance_to_park
+end
+binding.pry
+ final_stops_num = dist_same_line(line_1,stop_1,line_2,stop_2,subway_lines)
+puts "I traveled #{final_stops_num} stops"
+#used pry to get these
+#initial_stop = subway_lines[line_1.to_sym].index(stop_1).to_i
 
