0f7e8cbf7391f040db7a62adf34bc6c9fb398b56
Bobby Mazzotti <robert.mazzoti@gmail.com>
2013-10-06 17:58:47 -0400

    Recreated mbta file and adjusted arrays

--- mbta.rb
+++ mbta.rb
@@ -1,1 +1,47 @@
+require 'pry'
 # Your code goes here
+
+# Get user input for line_color and station.
+puts "What station are you starting your jounrey from? " 
+"/n Red: South Station, Park St, Kendall, Central, Harvard" 
+"/n Green: Haymarket, Goverment, Park St, Boylston, Arlington, Copley"
+"/n Orange: North Station, Park Street, State, Downtown Crossing, Chinatown, Back Bay, Forest Hills"
+begin_station = gets.chomp
+
+puts "What line are you starting your journey on? " "Green, Red, or Orange"
+begin_line = gets.chomp
+
+puts "What station will you end your journey with? "
+"/n Red: South Station, Park St, Kendall, Central, Harvard" 
+"/n Green: Haymarket, Goverment, Park St, Boylston, Arlington, Copley"
+"/n Orange: North Station, Park Street, State, Downtown Crossing, Chinatown, Back Bay,  Forest Hills"
+end_station = gets.chomp
+
+puts "What line will you end your journey on?" "/nGreen, /nRed, /nOrange"
+end_line = gets.chomp
+
+
+# Build an function array for each line and stop. 
+lines = {	
+	red: ["south_station", "park_st", "Kendall", "central", "harvard"],
+	green: ["haymarket", "goverment_center", "park_st", "boylston", "arlington", "copley"],
+	orange: ["north_station", "haymarket", "park_st", "state", "downtown_crossing", "chinatown", "back_bay", "forest_hills"]
+}
+
+def one_line_trip(begin_station, begin_line, end_station, end_line)
+	current_trip = []
+				current_trip = lines.fetch(begin_line)
+				dis_1 = current_trip.index(begin_station)
+				dis_2 = current_trip.index(end_station)
+				return (dis_1.to_i - dis_2.to_i).abs
+end
+
+#different line method
+def diff_line_distance(begin_station, begin_line, end_station, end_line, multi)
+	start = multi[start_line.to_sym].index(start_station).to_i
+	stop = multi[stop_line.to_sym].index(stop_station).to_i
+
+
+
+
+
---
+++ mbta2.rb
@@ -0,0 +1,39 @@
+puts "Please enter your starting line"
+	start_line = gets.chomp
+puts "Please enter your starting station"
+	start_station = gets.chomp
+puts "Please enter your stop line"
+	stop_line = gets.chomp
+puts "Please enter your stop station"
+	stop_station = gets.chomp
+
+lines = {
+  red: ["south_station", "park_street", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+  green: ["green_haymarket", "government_center", "park_street", "boylston", "arlington", "copley"],
+  orange: ["north_station", "orange_haymarket", "park_street", "state", "downtown_crossing", "chinatown", "back_bay", "forest_hills"]
+}
+
+
+#same line method
+def same_line_distance(start_line, start_station, stop_line, stop_station, array)
+	distance_one = array[start_line.to_sym].index(start_station).to_i
+	distance_two = array[stop_line.to_sym].index(stop_station).to_i
+	return (distance_one - distance_two).abs
+end
+ 
+
+#different line method
+def diff_line_distance(start_line, start_station, stop_line, stop_station, array)
+	start = array[start_line.to_sym].index(start_station).to_i
+	stop = array[stop_line.to_sym].index(stop_station).to_i
+	start_park_point = array[start_line.to_sym].index("park_street").to_i
+	stop_park_point = array[stop_line.to_sym].index("park_street").to_i
+	dis_to_park_start = (start - start_park_point).to_i
+	dis_to_park_stop = (stop - stop_park_point).to_i
+	return (dis_to_park_start + dis_to_park_stop)
+end
+
+if start_line == stop_line
+	puts same_line_distance(start_line, start_station, stop_line, stop_station, lines)
+	else puts diff_line_distance(start_line, start_station, stop_line, stop_station, lines)
+end
