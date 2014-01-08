c9b97187a7642f1304f089d9b6dc42922c371682
Mike Stone <mikestone14@gmail.com>
2014-01-08 18:40:07 -0500

    Completed HW using 2 methods for same line and different line distances

--- mbta.rb
+++ mbta.rb
@@ -7,6 +7,8 @@ red_line = ['South Station', 'Park St.', 'Kendall', 'Central', 'Harvard', 'Porte
 green_line = ['Haymarket', 'Government Center', 'Park St.', 'Boyleston', 'Arlington', 'Copley']
 orange_line = ['North Station', 'Haymarket', 'Park St.', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']
 
+
+
 # Get user input
 def user_input(prompt)
 	puts prompt
@@ -14,14 +16,14 @@ def user_input(prompt)
 end
 
 # Get the user's starting location
-start_line = user_input("What line are you beginning on? (type Red, Green, or Orange)").upcase
+start_line = user_input("What line are you beginning on? (type Red, Green, or Orange)").capitalize
 case start_line
-when "GREEN"
-	start_line = green_line
-when "RED"
-	start_line = red_line
-when "ORANGE"
-	start_line = orange_line
+when "Green"
+	start_line_array = green_line
+when "Red"
+	start_line_array = red_line
+when "Orange"
+	start_line_array = orange_line
 else
 	puts "Incorrect subway line. Goodbye"
 	exit
@@ -32,14 +34,14 @@ start_stop = user_input("What stop do you begin at?")
 puts ""
 
 # Get the user's ending location
-end_line = user_input("What line are you ending on?").upcase
+end_line = user_input("What line are you ending on?").capitalize
 case end_line
-when "GREEN"
-	end_line = green_line
-when "RED"
-	end_line = red_line
-when "ORANGE"
-	end_line = orange_line
+when "Green"
+	end_line_array = green_line
+when "Red"
+	end_line_array = red_line
+when "Orange"
+	end_line_array = orange_line
 else
 	puts "Incorrect subway line. Goodbye."
 	exit
@@ -58,14 +60,23 @@ def same_line_distance(start_stop, end_stop, subway_line)
 end
 
 # Calculate distance is subway lines change
-
-if start_line == end_line
-	distance = same_line_distance(start_stop, end_stop, start_line)
-	puts "The distance between #{start_stop} and #{end_stop} is #{distance} stops."
+# Calculate line distance to park
+def distance_to_park(subway_stop, subway_line)
+	start_index = subway_line.index("#{subway_stop}")
+	park_index = subway_line.index('Park St.')
+	start_index > park_index ? (start_index - park_index) : (park_index - start_index)
 end
 
+distance = distance_to_park(start_stop, start_line_array) +  distance_to_park(end_stop, end_line_array)
+puts "The distance between #{start_stop} on the #{start_line} Line and #{end_stop} on the #{end_line} Line is #{distance} stops."
 
-
-
+# Output the distance based on whether the start & end lines are the same or not
+if start_line == end_line
+	distance = same_line_distance(start_stop, end_stop, start_line_array)
+	puts "The distance between #{start_stop} and #{end_stop} is #{distance} stops."
+else
+	distance = distance_to_park(start_stop, start_line_array) +  distance_to_park(end_stop, end_line_array)
+	puts "The distance between #{start_stop} on the #{start_line} Line and #{end_stop} on the #{end_line} Line is #{distance} stops."
+end
 
 puts ""
