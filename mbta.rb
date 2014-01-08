ced556a965ebbb82df1ac5a35d8aea58dba60802
Jordan Morano <jordan.morano@gmail.com>
2014-01-08 18:30:11 -0500

    added the three t lines as arrays

--- mbta.rb
+++ mbta.rb
@@ -1,1 +1,7 @@
 # Your code goes here
+# Set up the three lines as arrays
+red_line = ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife']
+green_line = ['haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley']
+orange_line = ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
+
+
