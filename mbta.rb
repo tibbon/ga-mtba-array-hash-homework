764d6d723284bb8b0a68be7bc680c626f230c118
Laure Fischer <laurefischer27@gmail.com>
2014-01-08 19:27:36 -0500

    added a hash to represent the subway lines and defined a method to prompt the user for four inputs

--- mbta.rb
+++ mbta.rb
@@ -1,1 +1,16 @@
-# Your code goes here
+mbta_system = {
+  orange: ['Forest Hills', 'Back Bay', 'Chinatown', 'Downtown Crossing', 'State', 'Park St', 'Haymarket Orange', 'North Station']
+  red: ['South Station', 'Park St', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife']
+  green: ['Haymarket Green', 'Government Center', 'Park St', 'Boylston', 'Arlington', 'Copley']
+}
+
+def get_input(prompt)
+  puts prompt
+  gets.chomp
+end
+
+input_origin_line = get_input("Enter the T line of the stop at which you will be boarding the subway:")
+input_origin_stop = get_input("Enter the stop at which you will be boarding the subway:")
+
+input_destination_line = get_input ("Enter the T line of the stop at which you will be getting off:")
+input_destination_stop = get_input ("Enter the stop at which you will be getting off:")
