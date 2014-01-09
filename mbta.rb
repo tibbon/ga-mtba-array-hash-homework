7d45f40931670dd45b560cad5226ea15875cb097
K Mak <karen.mak@stx.oxon.org>
2014-01-08 21:36:08 -0500

    added prompt function and abs

--- mbta.rb
+++ mbta.rb
@@ -1,38 +1,37 @@
+
+  def get_input(prompt)
+    puts prompt
+    gets.chomp
+  end
+
+
 subway = {
-  :red => ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
-  :green => ['green_haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
-  :orange => ['north station', 'orange_haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']}
+  :red => ['South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+  :green => ['Haymarket', 'Government center', 'Park Street', 'Bolyston', 'Arlington', 'Copley'],
+  :orange => ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crossing', 'Chinatown', 'Back bay', 'Forest Hills']}
 
+# origin_station will be a string value within an array, we need to know its index position?
+origin_station = get_input("Welcome, please enter the name of the station you will be departing from: ")
+#origin_line will be a symbol, a key of a hash, with a value of an array
+origin_line = get_input("Which subway line is the station on?")
+# destination_station will be a string value within an array, we need to know its index position?
+destination_station = get_input("Please enter the station you are going to: ")
+destination_line = get_input("Which subway line is the station on?")
 
-puts "Welcome, please enter the name of the station you will be departing from: "
-print "Departing from: "
-origin_station = gets.chomp # origin_station will be a string value within an array, we need to know its index position?
-puts "Which subway line is the station on?"
-print "Subway line: "
-origin_line = gets.chomp #origin_line will be a symbol, a key of a hash, with a value of an array
-puts "Please enter the station you are going to."
-print "Arriving at: "
-destination_station = gets.chomp # destination_station will be a string value within an array, we need to know its index position?
-puts "Which subway line is the station on?"
-print "Subway line: "
-destination_line = gets.chomp
 # will you be changing at park st?
 # index of park st
+# array index begins at 0, so maybe it doesn't matter we have dup park st?
 changing_point = subway["#{origin_line}".to_sym].index("park st")
 arrival_point = subway["#{destination_line}".to_sym].index("#{destination_station}")
 departure_point = subway["#{origin_line}".to_sym].index("#{origin_station}")
 same_line_arrival_point = subway["#{destination_line}".to_sym].index("#{destination_station}")
-puts changing_point + arrival_point - 1 if "#{origin_line}" != "#{destination_line}"
-puts same_line_arrival_point - departure_point if "#{origin_line}" == "#{destination_line}"
+puts (changing_point - arrival_point).abs - 1 if "#{origin_line}" != "#{destination_line}"
+puts (same_line_arrival_point - departure_point).abs if "#{origin_line}" == "#{destination_line}"
+
 
 =begin
-puts subway["#{origin_line}".to_sym][(subway["#{origin_line}".to_sym].index("#{origin_station}")).to_i,8)
-subway["#{destination_line}".to_sym].index("park st")
-puts subway["#{destination_line}".to_sym].index("#{destination_station}")
-
+how to get absolute number?
 origin_line = :red
-array index begins at 0, so maybe it doesn't matter we have dup park st?
-puts subway[:red].uniq + subway[:orange].uniq
-index position of one stop calculate to index position of the other stop? what about  when it's backwards?
-array.reject {|x| x < 3}
+index position of one stop calculate to index position of the other stop? what about when it's backwards?
+
 =end
