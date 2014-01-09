9782fe12ac3e1422300c8383fa25a6d2580161f0
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-09 10:08:36 -0500

    last one i promise

--- mbta.rb
+++ mbta.rb
@@ -2,12 +2,20 @@ mtba = { red: ["south station", "park st", "kendall", "central", "harvard", "por
 		green: ["green/haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
 		orange: ["north", "station", "orange/haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
 }
+def find_inter(name)
+	name[:red].each do |i|
+		inter = i
+		name[:green].each do|j|
+			return inter if inter == j
+		end
+	end
+end
 
-def count_stop(b_line, b_stop, e_line, e_stop, name)
+def count_stop(b_line, b_stop, e_line, e_stop, name, inter)
 	if b_line == e_line
 		return	(name[b_line.to_sym].index(b_stop) - name[e_line.to_sym].index(e_stop)).abs
 	else
-		return (name[b_line.to_sym].index(b_stop) - name[b_line.to_sym].index("park st")).abs + (name[e_line.to_sym].index(e_stop) - name[e_line.to_sym].index("park st")).abs
+		return (name[b_line.to_sym].index(b_stop) - name[b_line.to_sym].index(inter)).abs + (name[e_line.to_sym].index(e_stop) - name[e_line.to_sym].index(inter)).abs
 	end
 end
 
@@ -20,6 +28,9 @@ end_line = gets.chomp
 puts "please input the stop you want to end:"
 end_stop = gets.chomp
 
-puts count_stop(begin_line, begin_stop, end_line, end_stop, mtba)
+inter = find_inter(mtba)
+puts count_stop(begin_line, begin_stop, end_line, end_stop, mtba, inter)
+
+
 
 
