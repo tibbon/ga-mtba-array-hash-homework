1df94fc0a85fb4c6885e857906466a2e5286a027
tsyan <anna.tsykalova@gmail.com>
2014-01-08 22:13:30 -0500

    rewrote input methods

--- mbta.rb
+++ mbta.rb
@@ -8,11 +8,50 @@ red_line = ["alewife", "davis", "porter", "harvard", "central", "kendall", "park
 orange_line = ["north station", "haymarket", "park st", "downtown crossing", "chinatown", "back bay", "forest hills"]
 green_line = ["haymarket", "gov't center", "park st", "boylston", "arlington", "copley"]
 
+the_subway = {red_line: red_line, orange_line: orange_line, green_line: green_line}
 
-start_line = get_input("What line are you starting on? (red, orange, green)")
-start_station = get_input("What station are you starting at?\n(red line: #{red_line.join(", ")})\n(orange line: #{orange_line.join(", ")})\n(green line: #{green_line.join(", ")})")
-end_line = get_input("What line are you going to? (red, orange, green)")
-end_station = get_input("What station are you getting off at?\n(red line: #{red_line.join(", ")})\n(orange line: #{orange_line.join(", ")})\n(green line: #{green_line.join(", ")})")
+
+def input_start_line
+	puts "What line are you starting on? (red, orange, green)"
+	gets.chomp
+	# return input + "_line" # not needed?
+end
+
+def input_start_station
+	puts "What station are you starting at?\n(red line: #{red_line.join(", ")})\n(orange line: #{orange_line.join(", ")})\n(green line: #{green_line.join(", ")})"
+	gets.chomp
+end
+
+def input_end_line
+	puts "What line are you going to? (red, orange, green)"
+	gets.chomp
+end
+
+def input_end_station
+	puts "What station are you getting off at?\n(red line: #{red_line.join(", ")})\n(orange line: #{orange_line.join(", ")})\n(green line: #{green_line.join(", ")})"
+	gets.chomp
+end
+
 
 # GOAL: PRINT TOTAL NUMBER OF STOPS FOR EACH TRIP
 
+=begin
+
+general idea:
+if start_line and end_line are the same
+	then calculate (position of end_station - position of start_station)
+else
+	find the position of park st
+	calculate (position of park st - position of start_station)
+	calculate (position of park st - position of end_station)
+	add them together
+	maybe subtract 1?
+end
+
+=end
+
+start_station.index("start_station")
+
+
+
+# rewrite whole subway as 6 joined lines (e.g. red to green one way, red to green the other way, etc.)?
