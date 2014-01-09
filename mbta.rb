709f25d02ab38409a4ec47cd602356140ef11677
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-09 00:26:16 -0500

    Final edition

--- mbta.rb
+++ mbta.rb
@@ -1,14 +1,23 @@
-mbta = { red: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+mtba = { red: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
 		green: ["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
 		orange: ["north", "station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
 }
-
-
 def count_stop(b_line, b_stop, e_line, e_stop, name)
 	if b_line == e_line
-		return	(name[:red].index(b_stop) - name[:red].index(e_stop)).abs
+		return	(string_array(b_line, name).index(b_stop) - string_array(e_line, name).index(e_stop)).abs + 1
+
 	else
-		return ((name[:b_line].index(b_stop)-name[:b_line].index("park st"))).abs+((name[:e_line].index(b_stop)-mbta[:e_line].index("park st"))).abs
+		return (string_array(b_line, name).index(b_stop) - string_array(e_line, name).index("park st")).abs + (string_array(e_line, name).index(e_stop) - string_array(e_line, name).index("park st")).abs + 1
+	end
+
+end
+
+def string_array(line, name)
+	if line == 'red'
+		return name[:red]
+	elsif line == 'green'
+		return name[:green]
+	else return name[:orange]
 	end
 end
 
@@ -21,6 +30,6 @@ end_line = gets.chomp
 puts "please input the stop you want to end:"
 end_stop = gets.chomp
 
-puts count_stop(begin_line, begin_stop, end_line, end_stop, mbta)
+puts count_stop(begin_line, begin_stop, end_line, end_stop, mtba)
 
 
