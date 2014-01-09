9a12df6e5797888760cb909754bee89eea58fd27
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-09 09:04:56 -0500

    Precised version

--- mbta.rb
+++ mbta.rb
@@ -1,23 +1,13 @@
 mtba = { red: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
-		green: ["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
-		orange: ["north", "station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
+		green: ["green/haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
+		orange: ["north", "station", "orange/haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
 }
+
 def count_stop(b_line, b_stop, e_line, e_stop, name)
 	if b_line == e_line
-		return	(string_array(b_line, name).index(b_stop) - string_array(e_line, name).index(e_stop)).abs + 1
-
+		return	(name[b_line.to_sym].index(b_stop) - name[e_line.to_sym].index(e_stop)).abs + 1
 	else
-		return (string_array(b_line, name).index(b_stop) - string_array(e_line, name).index("park st")).abs + (string_array(e_line, name).index(e_stop) - string_array(e_line, name).index("park st")).abs + 1
-	end
-
-end
-
-def string_array(line, name)
-	if line == 'red'
-		return name[:red]
-	elsif line == 'green'
-		return name[:green]
-	else return name[:orange]
+		return (name[b_line.to_sym].index(b_stop) - name[b_line.to_sym].index("park st")).abs + (name[e_line.to_sym].index(e_stop) - name[e_line.to_sym].index("park st")).abs + 1
 	end
 end
 
