3dfb18c25863d3353bb8899ac75dd97df5b86fdd
tsyan <anna.tsykalova@gmail.com>
2014-01-08 21:38:13 -0500

    defined subway lines as arrays

--- mbta.rb
+++ mbta.rb
@@ -4,8 +4,15 @@ def get_input(prompt)
 	gets.chomp # becomes returned value
 end
 
+red_line = ["alewife", "davis", "porter", "harvard", "central", "kendall", "park st", "south station"]
+orange_line = ["north station", "haymarket", "park st", "downtown crossing", "chinatown", "back bay", "forest hills"]
+green_line = ["haymarket", "gov't center", "park st", "boylston", "arlington", "copley"]
+
+
 start_line = get_input("What line are you starting on? (red, orange, green)")
-start_station = get_input("What station are you starting at?\n(red line: alewife, davis, porter, harvard, central, kendall, park st, south station)\n(orange line: north station, haymarket, park st, downtown crossing, chinatown, back bay, forest hills)\n(green line: haymarket, gov't center, park st, boylston, arlington, copley)")
+start_station = get_input("What station are you starting at?\n(red line: #{red_line.join(", ")})\n(orange line: #{orange_line.join(", ")})\n(green line: #{green_line.join(", ")})")
 end_line = get_input("What line are you going to? (red, orange, green)")
-end_station = get_input("What station are you going to?\n(red line: alewife, davis, porter, harvard, central, kendall, park st, south station)\n(orange line: north station, haymarket, park st, downtown crossing, chinatown, back bay, forest hills)\n(green line: haymarket, gov't center, park st, boylston, arlington, copley)")
+end_station = get_input("What station are you getting off at?\n(red line: #{red_line.join(", ")})\n(orange line: #{orange_line.join(", ")})\n(green line: #{green_line.join(", ")})")
+
+# GOAL: PRINT TOTAL NUMBER OF STOPS FOR EACH TRIP
 
